0af7f379a431c332268fbc64399bb13e36fa01bb
sssiion <rta2752775@gmail.com>
2024-11-05 17:57:58 +0900

    dd

--- server/server.js
+++ server/server.js
@@ -6,7 +6,6 @@ const connectDB = require('./config/db');
 const path = require('path');
 const authController = require('./controllers/authController');
 const gameController = require('./controllers/gameController');
-
 const app = express();
 const PORT = 6000; //process.env.PORT || 6000;
 
@@ -17,15 +16,15 @@ connectDB();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public','src','client')));
 
 // Routes
 app.post('/api/login', authController.login);
-app.post('/api/saveGame', gameController.saveGame);
-app.get('/api/loadGame', gameController.loadGame);
+app.post('/api/saveGame', gameController.getGameState);
+app.get('/api/loadGame', gameController.saveGameState);
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'webgl.html'));
   });
   
   // WebGL 페이지 HTML 서빙
